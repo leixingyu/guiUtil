2fde19947b7cb80b64cda80c764dae6156ffa5d1
leixingyu <techartlei@gmail.com>
2022-03-27 18:04:10 -0400

    add documentation and minor qol addition to getTextDialog.py

--- template/getTextDialog.py
+++ template/getTextDialog.py
@@ -2,10 +2,21 @@ from Qt import QtWidgets, QtCore, QtGui
 
 
 class GetTextDialog(QtWidgets.QDialog):
-    def __init__(self):
+    """
+    Custom pop-up dialog for entering a short text
+    """
+
+    def __init__(self, text='', title=''):
+        """
+        Initializing the dialog ui elements and connect signals
+
+        :param text: str. pre-displayed text
+        :param title: str. dialog title
+        """
         super(GetTextDialog, self).__init__()
 
-        self.ui_text_edit = QtWidgets.QLineEdit()
+        self.setWindowTitle(title)
+        self.ui_text_edit = QtWidgets.QLineEdit(text)
         self.ui_accept_btn = QtWidgets.QPushButton("Confirm")
 
         layout = QtWidgets.QGridLayout()
@@ -16,10 +27,21 @@ class GetTextDialog(QtWidgets.QDialog):
         self.ui_accept_btn.clicked.connect(self.on_click_accept)
 
     def closeEvent(self):
+        """
+        Overwrite the close event as it handles accept by default
+        """
         self.close()
 
     def on_click_accept(self):
+        """
+        Trigger accept event when clicking the confirm button
+        """
         self.accept()
 
     def get_text(self):
+        """
+        Get the text from text edit field
+
+        :return: str. text from text edit field
+        """
         return self.ui_text_edit.text()
