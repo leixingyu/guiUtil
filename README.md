b78c7603f71cd132e433832712714703b3718c4d
leixingyu <techartlei@gmail.com>
2022-03-26 22:20:22 -0400

    move around modules from previous projects

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# guiUtil
+<div align="center">
+<h1 align="center">Gui Utility</h1>
+
+  <p align="center">
+    A Python utility library for Qt UI creation purpose
+  </p>
+</div>
+
+### Prerequisites
+
+- [Qt](https://github.com/mottosso/Qt.py): a module that supports different
+python qt bindings (Only needed if you need UI support functionalities)
+    ```
+    pip install Qt.py
+    ```
---
+++ template/editTextDialog.py
@@ -0,0 +1,51 @@
+from Qt import QtWidgets, QtGui
+
+
+class EditTextDialog(QtWidgets.QDialog):
+    """
+    Custom pop-up dialog for editing text purpose, or getting long text input
+    """
+
+    def __init__(self, text='', title=''):
+        """
+        Initializing the dialog ui elements and connect signals
+
+        :param text: str. pre-displayed text
+        :param title: str. dialog title
+        """
+        super(EditTextDialog, self).__init__()
+
+        self.setWindowTitle(title)
+        self.ui_text_edit = QtWidgets.QPlainTextEdit(text)
+        self.ui_text_edit.setTabStopWidth(self.ui_text_edit.fontMetrics().width(' ') * 4)
+        self.ui_accept_button = QtWidgets.QPushButton("Confirm")
+
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.ui_text_edit, 0, 0)
+        layout.addWidget(self.ui_accept_button, 1, 0)
+
+        self.setLayout(layout)
+        self.ui_accept_button.clicked.connect(self.on_click_accept)
+
+    def closeEvent(self, event):
+        """
+        Overwrite the close event as it handles accept by default
+        """
+        self.close()
+
+    def on_click_accept(self):
+        """
+        Trigger accept event when clicking the confirm button
+        """
+        if self.ui_text_edit.toPlainText():
+            self.accept()
+        else:
+            print('value cannot be empty')
+
+    def get_text(self):
+        """
+        Get the text from text edit field
+
+        :return: str. text from text edit field
+        """
+        return self.ui_text_edit.toPlainText()
