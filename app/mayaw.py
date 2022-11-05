53594186ae08ccd03e6727e49f3c7111f5b38cb8
leixingyu <techartlei@gmail.com>
2022-11-05 09:46:01 -0400

    add a pretty crazy way to find maya window title in Win10

--- app/mayaw.py
+++ app/mayaw.py
@@ -38,3 +38,44 @@ def set_window_pos(child_only=0, x=0, y=0):
                 if top.windowTitle() == get_maya_main_window().windowTitle():
                     continue
                 top.move(x, y)
+
+
+def get_maya_window_titles():
+    """
+    A C-like function to find maya window titles in Windows
+
+    :return: {int: str}. a dictionary mapping of the process id of Maya as key
+                         and window title as value.
+    """
+    import ctypes
+
+    EnumWindows = ctypes.windll.user32.EnumWindows
+    EnumWindowsProc = ctypes.WINFUNCTYPE(
+        ctypes.c_bool,
+        ctypes.POINTER(ctypes.c_int),
+        ctypes.POINTER(ctypes.c_int)
+    )
+    GetWindowText = ctypes.windll.user32.GetWindowTextW
+    GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+    GetWindowProcessId = ctypes.windll.user32.GetWindowThreadProcessId
+    IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+
+    win_mapping = dict()
+
+    def find_maya_window(hwnd, lParam):
+        if IsWindowVisible(hwnd):
+            # window name
+            length = GetWindowTextLength(hwnd)
+            buff = ctypes.create_unicode_buffer(length+1)
+            GetWindowText(hwnd, buff, length+1)
+
+            if 'Autodesk Maya' in buff.value:
+                # pid
+                pid = ctypes.wintypes.DWORD()
+                GetWindowProcessId(hwnd, ctypes.byref(pid))
+                win_mapping[int(pid.value)] = buff.value
+        return True
+
+    # query the window title
+    EnumWindows(EnumWindowsProc(find_maya_window), 0)
+    return win_mapping.items()
